50f16f26e5d39fa5c12de398c6c324e8664898d5
Davis <derekdavis2013@gmail.com>
2017-05-20 12:41:27 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { Route } from 'react-router-dom';
+import rootReducer from './reducers';
+import { Home } from './components/pages';
+
+// Only grid and table styles
+import './style/bootstrap.css';
+
+const history = createHistory();
+
+const middleware = composeWithDevTools(
+    applyMiddleware(
+        routerMiddleware(history),
+        thunkMiddleware
+    )
+);
+
+const store = createStore(
+    rootReducer,
+    middleware
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                <Route path="/">
+                    <div>
+                        <Route exact path="/" component={Home} />
+                    </div>
+                </Route>
+            </div>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
